4498810959bb520e9e5448219dc2b93baa01008b
Your Name <you@example.com>
2019-11-05 09:19:51 -0800

    MySQL NodeJS Module

--- service-app/index.js
+++ service-app/index.js
@@ -1,3 +1,22 @@
+/*var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "CELLS_DEMO"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "SELECT * FROM CELLS_DEMO_DATA";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("CELLS_DEMO_DATA Record Read");
+  });
+});
+*/
 const express = require("express");
 const app = express();
 app.get('/endpoint',function(req,res){

87402401bccadf530bad3baa8a906fb2d90b7f39
Your Name <you@example.com>
2019-11-06 11:43:21 -0800

    First Full Cluster View

--- modulo-cluster/modulo-cluster.js
+++ modulo-cluster/modulo-cluster.js
@@ -71,16 +71,68 @@
       <div class = "dinamic-tbl">
         <table>
           <caption>CLUSTER</caption>
-          <tr>
-              <td class = "verde">Balance</td>
-              <td class = "blanco">Adeudo</td>
-              <td class = "rojo">Tasa</td>
-          </tr>
+            <tr>
+              <td class = "verde">TIPODEOPERACION</td>
+              <td class = "verde">PEM</td>
+              <td class = "verde">MONTO</td>
+              <td class = "verde">NACIONAL_INT</td>
+              <td class = "verde">ECI</td>
+              <td class = "verde">NIPOFF</td>
+              <td class = "verde">CVV2</td>
+              <td class = "verde">TJ_F_TJ_D</td>
+              <td class = "verde">NIVELCUENTA</td>
+              <td class = "verde">REINCIDENCIA</td>
+              <td class = "verde">SEGMENTO</td>
+              <td class = "verde">ANTIGUEDAD</td>
+              <td class = "verde">Q2</td>
+              <td class = "verde">DIASTRANSCURRIDOS</td>
+              <td class = "verde">NUMOVIMIENTOS</td>
+              <td class = "verde">DIFTIEMPOENTREMOVIMIENTOS</td>
+              <td class = "verde">MONTOF</td>
+              <td class = "verde">FRAUDE</td>
+              <td class = "verde">TP_TNP</td>
+              <td class = "verde">MISMOMONTO</td>
+              <td class = "verde">MISMAAFILIACION</td>
+              <td class = "verde">TIEMPOENTREMOVIMIENTOS</td>
+              <td class = "verde">SUMAMONTOMOVIMIENTOSSELECCIONADOS</td>
+              <td class = "verde">PATRONDESBLOQUEO</td>
+              <td class = "verde">AUTHCE</td>
+              <td class = "verde">DIFFECHAOPERACIONALTA</td>
+              <td class = "verde">SITUACION</td>
+              <td class = "verde">NORECONOCEPRODUCTO</td>
+              <td class = "verde">SINMOVIMIENTO</td>
+            </tr>
           <template is="dom-repeat" items="[[objectArray]]">
             <tr>
-  	      <td class = "verde">[[item.PEM]]</td>
-              <td class = "blanco">[[item.PEM]]</td>
-              <td class = "rojo">[[item.PEM]]</td>
+  	      <td class = "verde">[[item.TIPODEOPERACION]]</td>
+              <td class = "verde">[[item.PEM]]</td>
+              <td class = "verde">[[item.MONTO]]</td>
+              <td class = "verde">[[item.NACIONAL_INT]]</td>
+              <td class = "verde">[[item.ECI]]</td>
+              <td class = "verde">[[item.NIPOFF]]</td>
+              <td class = "verde">[[item.CVV2]]</td>
+              <td class = "verde">[[item.TJ_F_TJ_D]]</td>
+              <td class = "verde">[[item.NIVELCUENTA]]</td>
+              <td class = "verde">[[item.REINCIDENCIA]]</td>
+              <td class = "verde">[[item.SEGMENTO]]</td>
+              <td class = "verde">[[item.ANTIGUEDAD]]</td>
+              <td class = "verde">[[item.Q2]]</td>
+              <td class = "verde">[[item.DIASTRANSCURRIDOS]]</td>
+              <td class = "verde">[[item.NUMOVIMIENTOS]]</td>
+              <td class = "verde">[[item.DIFTIEMPOENTREMOVIMIENTOS]]</td>
+              <td class = "verde">[[item.MONTOF]]</td>
+              <td class = "verde">[[item.FRAUDE]]</td>
+              <td class = "verde">[[item.TP_TNP]]</td>
+              <td class = "verde">[[item.MISMOMONT]]O</td>
+              <td class = "verde">[[item.MISMAAFILIACION]]</td>
+              <td class = "verde">[[item.TIEMPOENTREMOVIMIENTOS]]</td>
+              <td class = "verde">[[item.SUMAMONTOMOVIMIENTOSSELECCIONADOS]]</$
+              <td class = "verde">[[item.PATRONDESBLOQUEO]]</td>
+              <td class = "verde">[[item.AUTHCE]]</td>
+              <td class = "verde">[[item.DIFFECHAOPERACIONALTA]]</td>
+              <td class = "verde">[[item.SITUACION]]</td>
+              <td class = "verde">[[item.NORECONOCEPRODUCTO]]</td>
+              <td class = "verde">[[item.SINMOVIMIENTO]]</td>
             </tr>
           <template>
         </table>
@@ -90,3 +142,4 @@
   }
   customElements.define(ModuloCluster.is, ModuloCluster);
 }
+
